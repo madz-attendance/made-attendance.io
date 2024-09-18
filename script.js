6a4697a61d19243149b4021f532537c821c24475
madz-attendance <159331685+madz-attendance@users.noreply.github.com>
2024-09-18 08:15:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const supabasePublicClient = supabase.createClient('https://agldqgjpcqqmqynizbcs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnbGRxZ2pwY3FxbXF5bml6YmNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5NDA5OTgsImV4cCI6MjA0MDUxNjk5OH0.qIUhU-16YQzrIY_SnRxWDG3l5RzKj9s8ns3XaoQAEFo')
 
 //const supabasePublicClient = supabase.createClient('https://agldqgjpcqqmqynizbcs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnbGRxZ2pwY3FxbXF5bml6YmNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5NDA5OTgsImV4cCI6MjA0MDUxNjk5OH0.qIUhU-16YQzrIY_SnRxWDG3l5RzKj9s8ns3XaoQAEFo')
-
+/*
 // When the button is clicked
 document.getElementById('myButton').addEventListener('click', async function() {
 // Set message to "Button clicked"
@@ -29,6 +29,9 @@ const { error2 } = await supabasePublicClient
 // will be a "conflict" and not work. This is intended to make sure that the primary key, id, stays unique
 
 });
+*/
+
+
 
 /*
 // Function to initialize Supabase and update page data
