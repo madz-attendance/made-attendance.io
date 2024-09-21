47fc8161ecf7db1247513463969c000fa534c7f1
madz-attendance <159331685+madz-attendance@users.noreply.github.com>
2024-09-20 23:26:09 -0400

    Create supabaseInit.js

---
+++ supabaseInit.js
@@ -0,0 +1,1 @@
+const supabasePublicClient = supabase.createClient('https://agldqgjpcqqmqynizbcs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnbGRxZ2pwY3FxbXF5bml6YmNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5NDA5OTgsImV4cCI6MjA0MDUxNjk5OH0.qIUhU-16YQzrIY_SnRxWDG3l5RzKj9s8ns3XaoQAEFo')
