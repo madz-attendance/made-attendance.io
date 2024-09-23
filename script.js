9d2557c0daae817a0a9556d3572016b6b359b717
DavidJJ3715 <152301752+DavidJJ3715@users.noreply.github.com>
2024-09-23 15:20:40 -0400

    Update script.js
    Added the ability to press enter on the sign in screen

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const supabasePublicClient = supabase.createClient('https://agldqgjpcqqmqynizbcs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnbGRxZ2pwY3FxbXF5bml6YmNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5NDA5OTgsImV4cCI6MjA0MDUxNjk5OH0.qIUhU-16YQzrIY_SnRxWDG3l5RzKj9s8ns3XaoQAEFo')
 
+document.getElementById('sign_in').addEventListener('keydown', function(event) { //Can press enter to sign in
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent form submission if inside a form
+        signIn(); // Call the signIn function
+    }
+});
+
 // Handle DOM content and tab functionality
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.question button');
