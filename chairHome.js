c192a896f187e69d18db3bef580740f7dea96782
mlici <159051058+mlici@users.noreply.github.com>
2024-10-01 01:54:05 -0400

    Update chairHome.js

--- chairHome.js
+++ chairHome.js
@@ -1,4 +1,4 @@
-//Mark 10/1/24
+//Mark addition 10/1/24
 document.getElementById('sign_in').addEventListener('keydown', function(event) { //Can press enter to sign in
     if (event.key === 'Enter') {
         event.preventDefault(); // Prevent form submission if inside a form
